c4e7351c3e19e7e0094202449934785ec042021e
AlinaBielska <alinkabelka5@gmail.com>
2023-03-14 21:51:02 +0100

    zadanie 1 + zadanie 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,53 @@
+import vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new vimeo('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+    const onPlay = function(e) {
+        const savedTime = e.seconds;
+        saveInLocalStorage(LOCALSTORAGE_KEY, savedTime);
+    };
+
+
+const saveInLocalStorage = (key, value) => {
+  try {
+    const dataInLocalStorage = JSON.stringify(value);
+    localStorage.setItem(key, dataInLocalStorage);
+  } catch (error) {
+    console.error("Return error: ", error.message);
+  }
+};
+
+
+player.on('timeupdate', onPlay);
+
+
+const loadFromLocaleStorage = key => {
+  try {
+    const dataInLocalStorage = localStorage.getItem(key);
+    return dataInLocalStorage === null ? undefined : JSON.parse(dataInLocalStorage);
+  } catch (error) {
+    console.error("Return errorr: ", error.message);
+  }
+};
+
+
+export default {
+  saveInLocalStorage,
+  loadFromLocaleStorage,
+};
+
+
+const savedTime = loadFromLocaleStorage(LOCALSTORAGE_KEY);
+
+player.setCurrentTime(savedTime).then(function(seconds){}).catch(function(error) {});
