16ccce4da7524b6ec35260938cd1363e42e518af
AlinaBielska <alinkabelka5@gmail.com>
2023-03-15 20:04:48 +0100

    zadanie 3 - start

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -503,6 +503,41 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"eH52W":[function(require,module,exports) {
+const form = document.querySelector("form");
+const KEY_STORAGE = "feedback-form-state";
+let formMemory = {};
+form.addEventListener("input", setLocalStorage);
+const setLocalStorage = (e)=>{
+    e.preventDefault();
+    saveLocalStorage(KEY_STORAGE, formMemory);
+    formMemory[e.target.name] = e.target.value;
+};
+const saveLocalStorage = (key, value)=>{
+    try {
+        const serializedState = JSON.stringify(value);
+        localStorage.setItem(key, serializedState);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+const formFromStorage = loadLocalStorage(KEY_STORAGE);
+const loadLocalStorage = (key)=>{
+    try {
+        const serializedState = localStorage.getItem(key);
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+//uzupełnić pola w formularzu
+if (formFromStorage) {
+    for(const key in formFromStorage);
+}
+form.addEventListener("submit", cleanForm);
+const cleanForm = (e)=>{
+    console.log(formMemory);
+    localStorage.removeItem("KEY_STORAGE");
+};
 
 },{}]},["7Ih8d","eH52W"], "eH52W", "parcelRequired7c6")
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+const form = document.querySelector("form");
+const KEY_STORAGE = "feedback-form-state";
 
+let formMemory = {};
+
+form.addEventListener("input", setLocalStorage);
+
+const setLocalStorage = e => {
+    e.preventDefault();
+    saveLocalStorage(KEY_STORAGE, formMemory);
+    formMemory[e.target.name] = e.target.value;
+}
+
+const saveLocalStorage = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const formFromStorage = loadLocalStorage(KEY_STORAGE);
+
+const loadLocalStorage = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+//uzupełnić pola w formularzu
+if (formFromStorage) {
+    for (const key in formFromStorage) {
+        
+    }
+}
+
+form.addEventListener("submit", cleanForm);
+
+const cleanForm = e => {
+    console.log(formMemory);
+    localStorage.removeItem("KEY_STORAGE");
+    
+}
